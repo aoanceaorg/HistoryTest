2d5f517f2bbbb2cd38f5cbcf20c7b805f848dd41
MarianCristian <maryan109@yahoo.ro>
2016-09-12 09:55:59 +0300

    Third commit

--- HistoryTest/TestFolder1/TestClass.cs
+++ HistoryTest/TestFolder1/TestClass.cs
@@ -15,5 +15,53 @@ namespace HistoryTest.TestFolder1
         {
 
         }
+        public void Foo1233312()
+        {
+
+        }
+        public void Boo44()
+        {
+
+        }
+        public void Foo123()
+        {
+
+        }
+        public void Boo323()
+        {
+
+        }
+        public void Foo1232312()
+        {
+
+        }
+        public void Boo123321()
+        {
+
+        }
+        public void Foo12()
+        {
+
+        }
+        public void Boo22()
+        {
+
+        }
+        public void Foo3()
+        {
+
+        }
+        public void Boo1()
+        {
+
+        }
+        public void Foo2()
+        {
+
+        }
+        public void Boo4()
+        {
+
+        }
     }
 }
