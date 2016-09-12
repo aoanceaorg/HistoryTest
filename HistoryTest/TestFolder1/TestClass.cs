bbef011d3e6deaf58d913c219181d0e6bf7dfb59
Marian Cristian <marian.serban@qubiz.com>
2016-09-12 10:02:31 +0300

    Test branch Edit (#2)
    *Edit Pr

---
+++ HistoryTest/TestFolder1/TestClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HistoryTest.TestFolder1
+{
+    public class TestClass
+    {
+        public void Foo()
+        {
+
+        }
+        public void Boo()
+        {
+
+        }
+        public void Foo1233312()
+        {
+
+        }
+        public void Boo44()
+        {
+
+        }
+        public void Foo123()
+        {
+
+        }
+        public void Boo323()
+        {
+
+        }
+        public void Foo1232312()
+        {
+
+        }
+        public void Boo123321()
+        {
+
+        }
+        public void Foo12()
+        {
+
+        }
+        public void Boo22()
+        {
+
+        }
+        public void Foo3()
+        {
+
+        }
+        public void Boo1()
+        {
+
+        }
+        public void Foo2()
+        {
+
+        }
+        public void Boo4()
+        {
+
+        }
+    }
+}
